df35ca377a6420d8e0d36b4c435a94656794c0cf
Franck Asket <32412954+asketsystem@users.noreply.github.com>
2022-10-15 04:57:16 +0000

    Express Web server

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,10 @@
-console.log('Hello world!')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hi API !')
+})
+
+app.listen(3000, () => {
+    console.log('server is running on port http://localhost:3000')
+})
