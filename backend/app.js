6db3414a5d725e8b251f7131fbccb7b17f0633c1
Franck Asket <32412954+asketsystem@users.noreply.github.com>
2022-10-15 06:20:53 +0000

    Installing Mongoose & Connect to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 require('dotenv/config')
 
@@ -11,20 +12,33 @@ app.use(express.json())
 app.use(morgan('tiny'))
 
 app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url',
-    }
-    res.send(product)
+  const product = {
+    id: 1,
+    name: 'hair dresser',
+    image: 'some_url',
+  }
+  res.send(product)
 })
 
 app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body
-    console.log(newProduct)
-    res.send(newProduct)
+  const newProduct = req.body
+  console.log(newProduct)
+  res.send(newProduct)
 })
 
+mongoose
+  .connect(process.env.CONNECT_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'e-shop',
+  })
+  .then(() => {
+    console.log('DB Connection is ready...')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+
 app.listen(3000, () => {
-    console.log('server is running on port http://localhost:3000')
+  console.log('server is running on port http://localhost:3000')
 })
