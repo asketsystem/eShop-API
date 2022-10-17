3e0474810cd7c2a902be781d9507948b6c70dd55
Franck Asket <32412954+asketsystem@users.noreply.github.com>
2022-10-17 23:35:05 +0000

    Get Products Count for Statistics Purposes

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -95,4 +95,15 @@ router.delete('/:id', (req, res) => {
     })
 })
 
+router.get(`/get/count`, async (req, res) => {
+  const productCount = await Product.countDocuments((count) => count)
+
+  if (!productCount) {
+    res.status(500).json({ success: false })
+  }
+  res.send({
+    productCount: productCount,
+  })
+})
+
 module.exports = router
